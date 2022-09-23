82a5798a3f72c66a9e53668eb5e5f1a943f55a34
1Sweat4Fun <112199209+1Sweat4Fun@users.noreply.github.com>
2022-09-23 13:33:54 -0500

    add class-9

---
+++ Class-9/Class-9/index.js
@@ -0,0 +1,12 @@
+fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+  .then((response) => response.json())
+  .then((data) => console.log(data){
+//Step 1 Access the img element
+let imageElement = document.getElementById("random-imgae");
+
+
+
+
+
+
+  });
